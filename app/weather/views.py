84bda9d15ed7a0c4e59904a15d9bb72a8c5fbe08
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-06-24 13:31:18 +0200

    Added logging and weather fetching from template

--- app/weather/views.py
+++ app/weather/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from . import weather_api
+
+import logging
+
+logging.basicConfig(
+  level=logging.INFO,
+  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+  filename='logs/weather_views.log',
+  )
 
 # Create your views here.
 def weather_view(request):
-    return render(request, 'weather/weather.html')
+    logging.info('Calling weather_view')
+    weather = weather_api.getweather()
+    logging.debug(f'Weather: {weather!r}')
+    logging.info('Redirecting to weather/weather.html')
+    return render(request, 'weather/weather.html', {'weather': weather})
--- app/weather/weather_api.py
+++ app/weather/weather_api.py
@@ -4,13 +4,22 @@ import python_weather
 import asyncio
 import os
 
-async def getweather(city_name, unit):
+import logging
+logging.basicConfig(
+  level=logging.INFO,
+  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+  filename='logs/weather_api.log',
+  )
+
+async def getweather(city_name='New York', unit=python_weather.METRIC):
+  logging.info(f'Calling getweather with\ncity_name={city_name}\nunit={unit}')
   # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
   async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
     # fetch a weather forecast from a city
     weather = await client.get(location=city_name, unit=unit)
+    logging.debug(f'Weather: {weather!r}')
 
-    # returns the current day's forecast temperature (int)
+    '''# returns the current day's forecast temperature (int)
     print(f'Temperature: {weather.temperature}')
 
     # get the weather forecast for a few days
@@ -19,7 +28,9 @@ async def getweather(city_name, unit):
 
       # hourly forecasts
       for hourly in daily.hourly_forecasts:
-        print(f' --> {hourly!r}')
+        print(f' --> {hourly!r}')'''
+        
+    return weather
 
 if __name__ == '__main__':
   # see https://stackoverflow.com/questions/45600579/asyncio-event-loop-is-closed-when-getting-loop
@@ -29,4 +40,5 @@ if __name__ == '__main__':
 
   input_city = input("Enter the city name: ")
   unit = python_weather.METRIC
-  asyncio.run(getweather(city_name=input_city, unit=unit))
+  w = asyncio.run(getweather(city_name=input_city, unit=unit))
+  print(w)
