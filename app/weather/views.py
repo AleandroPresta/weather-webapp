531f3acfcff5e249177445ae102b11f074c5f278
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-06-28 18:21:50 +0200

    Created shell feature of hourly_forecasts

--- app/weather/views.py
+++ app/weather/views.py
@@ -9,6 +9,7 @@ def weather_view(request):
     logging.info('Calling weather_view')
     forecast = weather_api.fetch()
     daily_forecasts = extract_daily_forecasts(forecast['daily_forecasts'])
+    today_hourly_forecasts = extract_hourly_forecasts(daily_forecasts[0]['hourly_forecasts']) # daily_forecasts[0] is today's forecast
     ic(daily_forecasts)
     logging.info('Redirecting to weather/weather.html')
     return render(
@@ -17,7 +18,9 @@ def weather_view(request):
         {
             'forecast': forecast,
             'daily_forecasts': daily_forecasts,
+            'today_hourly_forecasts': today_hourly_forecasts,
         })
+    
     
 '''
     Extracts the daily forecasts as list of dictionaries from the generator
@@ -36,3 +39,7 @@ def extract_daily_forecasts(daily_forecasts):
 
     
     return result
+
+
+def extract_hourly_forecasts(daily_forecast):
+    pass
